d8af027f87b723468e639e34638e5990789284ed
lfabisiak <46755831+lfabisiak@users.noreply.github.com>
2019-05-10 11:17:14 +0200

    init commit

---
+++ Sandbox/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1041;
+            Console.WriteLine(Convert.ToString(n, 2));
+            Console.WriteLine(Task.Solution(n));
+            
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Sandbox/Task.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace Sandbox
+{
+    class Task
+    {
+        public static int Solution(int n)
+        {
+            StringBuilder binary = new StringBuilder(Convert.ToString(n, 2));
+
+            if (binary.Length > 2)
+            {
+                ArrayList lenghtsOfBinaryGaps = new ArrayList();
+
+                for (int i = 0; i < binary.Length; i++)
+                {
+                    if (binary[i].Equals('1') && i+1 < binary.Length && binary[i + 1].Equals('0'))
+                    {
+                        int lenghtOfBinaryGap = 0;
+
+                        for (int j = i + 1; j < binary.Length; j++)
+                        {
+
+                            if (binary[j].Equals('0'))
+                            {
+                                lenghtOfBinaryGap++;
+                                i++;
+
+                            }
+
+                            else
+                            {
+                                lenghtsOfBinaryGaps.Add(lenghtOfBinaryGap);
+                                break;
+                            }
+
+                        }
+
+                    }
+
+                }
+
+                if(lenghtsOfBinaryGaps.Count > 0)
+                {
+                    lenghtsOfBinaryGaps.Sort();
+                    lenghtsOfBinaryGaps.Reverse();
+
+                    return (int)lenghtsOfBinaryGaps[0];
+                }
+                
+                else
+                {
+                    return 0;
+                }
+
+            } else
+
+            {
+                return 0;
+            }
+
+            
+        }
+    }
+}
